1170154244552280369a719a1ce01d7fbb8c862a
Mostafa Mahmoud <mostafamahmoud.cse@gmail.com>
2023-08-25 21:05:32 +0300

    application files

---
+++ Unit 3 (Embedded C)/Lesson 2/Lab_1/app.c
@@ -0,0 +1,14 @@
+#include "uart.h"
+
+// Note: 100 Bytes in decimal -> 64 in hexa
+
+// Stored inside the .data section
+unsigned char string_buffer1[100] = "Learn-in-depth:<Mostafa Mahmoud>";
+
+// Stored inside the .rodata section
+unsigned char const string_buffer2[100] = "Learn-in-depth:<Mostafa Mahmoud>";
+
+void main(void)
+{
+    UART_sendString(string_buffer1);
+}
